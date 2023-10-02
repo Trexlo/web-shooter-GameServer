642543444a2b4a373e51922ba056bed72e7c2b2d
Trexlo <146723007+Trexlo@users.noreply.github.com>
2023-10-02 20:28:15 +0200

    http

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-const https = require('https');
-const server = https.createServer();
+const http = require('http');
+const server = http.createServer();
 const { Server } = require("socket.io");
 const THREE = require('three');
 const { io } = require('socket.io-client');
